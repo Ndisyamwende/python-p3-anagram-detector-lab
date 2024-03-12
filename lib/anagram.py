8ae155e7452af112245779bafba004a6ca9976cc
Ndisyamwende <sharonndisya2002@gmail.com>
2024-03-12 12:43:41 +0300

    passed tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        lowercase_word = self.word.lower()
+        return [anagram for anagram in possible_anagrams if self._is_anagram(lowercase_word, anagram.lower())]
+
+    def _is_anagram(self, word1, word2):
+        return sorted(word1) == sorted(word2)
